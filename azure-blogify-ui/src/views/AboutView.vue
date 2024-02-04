3543e081c664243ec2d5c9a864b27e1e29ac3490
Brad Malgas <bradstermalgas@gmail.com>
2024-02-04 13:01:31 +0200

    Created project scaffold with VueJS + TailWind CSS

---
+++ azure-blogify-ui/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="text-3xl">Azure Blogify</div>
+  <p>This is the About page that will contain information about me.</p>
+  <div>
+    <div><router-link to="/about">Go to About</router-link></div>
+    <div><router-link to="/">Go to Home</router-link></div>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ azure-blogify-ui/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+
+const store = useCounterStore()
+const increment = () => { store.increment() }
+</script>
+
+<template>
+  <main>
+  <button @click="increment">Add count</button>    
+  <div class="text-3xl">Azure Blogify | Page Vists = {{store.count}}</div>
+  <p>This is the Home page that will contain all the latest blog posts.</p>
+  <div>
+    <div><router-link to="/about">Go to About</router-link></div>
+    <div><router-link to="/">Go to Home</router-link></div>
+  </div>
+  </main>
+</template>
