3a6ef46e3a7d49f300ad7e4a0e78bbbaacf5af07
Brad Malgas <bradstermalgas@gmail.com>
2024-02-15 22:10:25 +0200

    added mock footer

---
+++ azure-blogify-ui/src/components/FooterInfo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import LogoMedium from './icons/LogoMedium.vue';
+</script>
+
+<template>
+    <div class="footer">
+        <div class="float-right">
+            <router-link to="/" class="nav-bar-item">Home</router-link>
+            <router-link to="/about" class="nav-bar-item">About</router-link>            
+            <input type="text" placeholder="Search.." class="searchbar">
+        </div>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.nav-bar-item{
+    @apply float-left text-center p-3 text-lg rounded text-white hover:text-blue-300
+}
+
+.searchbar{
+    @apply float-right p-1 mt-2 mr-4 text-lg
+}
+
+.footer{
+    @apply fixed w-full bottom-0 left-0 bg-blue-950 py-3 truncate
+}
+</style>
