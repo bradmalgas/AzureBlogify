ad5ae9ab8a0e828d1592266b97fdd903d3ee4a40
Brad Malgas <bradstermalgas@gmail.com>
2024-02-15 22:11:20 +0200

    Updated UI to include footer

--- azure-blogify-ui/src/App.vue
+++ azure-blogify-ui/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
+import FooterInfo from './components/FooterInfo.vue';
 </script>
 
 <template>
   <NavBar/>
   <RouterView />
+  <FooterInfo/>
 </template>
 
 <style scoped>
