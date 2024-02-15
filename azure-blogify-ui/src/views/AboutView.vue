e7ba84f895750b9a4d95dea6f55bff56b18bef23
Brad Malgas <bradstermalgas@gmail.com>
2024-02-15 16:32:09 +0200

    Removed n page router links

--- azure-blogify-ui/src/views/AboutView.vue
+++ azure-blogify-ui/src/views/AboutView.vue
@@ -4,10 +4,6 @@
 <template>
   <div class="text-3xl">Azure Blogify</div>
   <p>This is the About page that will contain information about me.</p>
-  <div>
-    <div><router-link to="/about">Go to About</router-link></div>
-    <div><router-link to="/">Go to Home</router-link></div>
-  </div>
 </template>
 
 <style>
--- azure-blogify-ui/src/views/HomeView.vue
+++ azure-blogify-ui/src/views/HomeView.vue
@@ -6,13 +6,9 @@ const increment = () => { store.increment() }
 </script>
 
 <template>
-  <main>
-  <button @click="increment">Add count</button>    
-  <div class="text-3xl">Azure Blogify | Page Vists = {{store.count}}</div>
+  <main>  
+  <div class="text-3xl">Page Vists = {{store.count}} </div>  
+  <button @click="increment" class="border-solid border-2 border-sky-500">Add count</button>  
   <p>This is the Home page that will contain all the latest blog posts.</p>
-  <div>
-    <div><router-link to="/about">Go to About</router-link></div>
-    <div><router-link to="/">Go to Home</router-link></div>
-  </div>
   </main>
 </template>
