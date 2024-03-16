4d11723308f5cfa100507b3a10a928709a1d0caf
Brad Malgas <bradstermalgas@gmail.com>
2024-03-16 22:24:46 +0200

    Fully responseive navbar

--- azure-blogify-ui/src/components/NavBar.vue
+++ azure-blogify-ui/src/components/NavBar.vue
@@ -2,29 +2,54 @@
 import LogoSmall from './icons/LogoSmall.vue';
 import LogoMedium from './icons/LogoMedium.vue';
 import LogoLarge from './icons/LogoLarge.vue';
+import SearchIcon from './icons/SearchIcon.vue';
+import SideNav from './SideNav.vue';
 </script>
 
 <template>
-    <div class="bg-blue-950 py-3 truncate">
-        <router-link to="/" class="float-left p-3">
-            <div class="flex items-center">
-            <LogoSmall/>
-            <h1 class="text-xl text-white">Azure Blogify</h1></div>
-        </router-link>
-        <div class="float-right">
-            <router-link to="/" class="nav-bar-item">Home</router-link>
-            <router-link to="/about" class="nav-bar-item">About</router-link>            
-            <input type="text" placeholder="Search.." class="searchbar">
+    <div class="bg-babyblue truncate hidden md:flex justify-between items-center">
+        <div class="px-3 lg:hidden">
+            <SideNav class="icon" />
+        </div>
+        <div>
+            <router-link to="/" class="">
+                <div class="flex items-center">
+                    <LogoSmall class="logo h-full w-full object-cover" />
+                    <h1 class="text-xl font-semibold">AzureBlogify</h1>
+                </div>
+            </router-link>
+        </div>
+        <div class="hidden lg:flex space-x-6 ml-auto mr-20">
+            <router-link to="/" key="home">Home</router-link>
+            <router-link to="/about" key="about">About</router-link>
+            <router-link to="/posts" key="posts">Post</router-link>
+            <router-link to="/about" key="contact">Contact</router-link>
+        </div>
+        <div class=" flex bg-gray-200 rounded-[13px] p-1 mr-3">
+            <input class="bg-gray-200 rounded-[13px] min-w-52" type="text" placeholder="Search..">
+            <SearchIcon class="h-8 pr-2" />
+        </div>
+    </div>
+    <div class="flex justify-between items-center bg-babyblue overflow-hidden md:hidden min-w-80">
+        <div class="px-3">
+            <SideNav />
+        </div>
+        <div class="flex items-center relative h-full">
+            <LogoSmall class="logo h-full w-full object-cover" />
+            <p class="text-lg font-semibold">AzureBlogify</p>
+        </div>
+        <div class="px-3">
+            <SearchIcon class="h-8 pr-2" />
         </div>
     </div>
 </template>
 
 <style lang="postcss" scoped>
-.nav-bar-item{
+.nav-bar-item {
     @apply float-left text-center p-3 text-lg rounded text-white hover:text-blue-300
 }
 
-.searchbar{
-    @apply float-right p-1 mt-2 mr-4 text-lg
+.logo {
+    max-height: 56px;
 }
 </style>
