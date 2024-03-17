e719b57ba5a4e1ebde4327c14ded3cfa45868114
Brad Malgas <bradstermalgas@gmail.com>
2024-03-17 16:08:07 +0200

    Added test post items

--- azure-blogify-ui/src/views/HomeView.vue
+++ azure-blogify-ui/src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import FeaturedPost from '@/components/FeaturedPost.vue';
+import PostItem from '@/components/PostItem.vue';
 
 </script>
 
 <template>
-  <div class="mx-10">
-    <div class="flex justify-center">
-      <FeaturedPost class="pt-3"/>
+  <div class="flex flex-col items-center pt-11 mx-10">
+    <div class="space-y-3">
+      <FeaturedPost/>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
     </div>
   </div>
 </template>
