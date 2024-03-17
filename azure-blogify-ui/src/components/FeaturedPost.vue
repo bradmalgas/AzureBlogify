5c99b146af07c79a0333160ccdf78dbcd3233682
Brad Malgas <bradstermalgas@gmail.com>
2024-03-17 13:48:59 +0200

    Added tablet+desktop view of featured post

--- azure-blogify-ui/src/components/FeaturedPost.vue
+++ azure-blogify-ui/src/components/FeaturedPost.vue
@@ -3,10 +3,10 @@
 
 <template>
     <div>
-        <h1 class="text-2xl font-bold mb-2">Featured Post</h1>
-        <img src="../assets/images/placeholder-image.png" alt="">
-        <h2 class="pb-1 font-bold leading-tight">Post title that is longer and more realistic of me</h2>
-        <p class="text-xs">13 May 2024</p>
+        <h1 class="md:text-3xl text-2xl font-bold mb-2">Featured Post</h1>
+        <div class="max-h-[300px] max-w-full overflow-clip"><img class="max-w-full h-auto" src="../assets/images/placeholder-image.png" alt=""></div>
+        <h2 class="md:text-xl my-1 font-bold leading-tight">Post title that is longer and more realistic of me</h2>
+        <p class="md:text-s text-xs">13 May 2024</p>
     </div>
 </template>
 
