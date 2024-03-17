ba8b530d7a357d03dc902ba8caf4d79240f70ddd
Brad Malgas <bradstermalgas@gmail.com>
2024-03-17 16:07:44 +0200

    Updated featured post on medium screen

--- azure-blogify-ui/src/components/FeaturedPost.vue
+++ azure-blogify-ui/src/components/FeaturedPost.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-    <div>
+    <div class="md:mb-6">
         <h1 class="md:text-3xl text-2xl font-bold mb-2">Featured Post</h1>
         <div class="max-h-[300px] max-w-full overflow-clip"><img class="max-w-full h-auto" src="../assets/images/placeholder-image.png" alt=""></div>
         <h2 class="md:text-xl my-1 font-bold leading-tight">Post title that is longer and more realistic of me</h2>
-        <p class="md:text-s text-xs">13 May 2024</p>
+        <p class="md:text-sm text-xs">13 May 2024</p>
     </div>
 </template>
 
