7d8d072de69d1abf26fbff8041876a512d31aad5
Brad Malgas <bradstermalgas@gmail.com>
2024-06-05 22:46:30 +0200

    Use blob storage for images

--- azure-blogify-ui/src/components/FeaturedPost.vue
+++ azure-blogify-ui/src/components/FeaturedPost.vue
@@ -5,7 +5,7 @@ defineProps(['title', 'date'])
 <template>
     <div class="md:mb-6">
         <h1 class="md:text-3xl text-2xl font-bold mb-2">Featured Post</h1>
-        <div class="max-h-[300px] max-w-full overflow-clip"><img class="max-w-full h-auto" src="../assets/images/placeholder-image.png" alt=""></div>
+        <div class="max-h-[300px] max-w-full overflow-clip"><img class="max-w-full h-auto" src="https://storageazureblogify.blob.core.windows.net/blogimages/placeholder-image.png" alt=""></div>
         <h2 class="lg:max-w-[600px] max-w-[510px] sm:max-sm:max-w-[200px] md:text-xl my-1 font-bold leading-tight">{{title}}</h2>
         <p class="md:text-sm text-xs">{{date}}</p>
     </div>
