9e7d2029a9342d4a1cc5ed7b94d87e47d44c7992
Brad Malgas <bradstermalgas@gmail.com>
2024-06-06 17:19:32 +0200

    added Post to seperate file

---
+++ azure-blogify-ui/api/models/Post.cs
@@ -0,0 +1,11 @@
+using System;
+
+public record Post
+{
+    public Guid id { get; set; }
+    public string title { get; set; }
+    public string? Author { get; set; }
+    public string category { get; set; }
+    public DateTime date { get; set; }
+    public string? Content { get; set; }
+}
