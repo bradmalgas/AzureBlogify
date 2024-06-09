ac012c180cfb774cf07943ea85b2717331e9cdbd
Brad Malgas <bradstermalgas@gmail.com>
2024-06-09 15:29:13 +0200

    Added infite scroll (load more) for posts

--- azure-blogify-ui/src/views/HomeView.vue
+++ azure-blogify-ui/src/views/HomeView.vue
@@ -3,20 +3,25 @@ import FeaturedPost from '@/components/FeaturedPost.vue';
 import PostListItem from '@/components/PostListItem.vue';
 import { onMounted, ref } from 'vue';
 
-const responseData = ref(null);
+const responseData = ref([]);
+const continuationToken = ref(null);
+const pageSize = ref(10);
 const error = ref(null);
 const loading = ref(false);
 
 const fetchData = async () => {
   loading.value = true;
   try {
-    const response = await fetch("/api/GetPosts");
+    let url = `/api/GetPosts?pageSize=${pageSize.value}`;
+    if (continuationToken.value) url += `&contToken=${continuationToken.value}`;
+    const response = await fetch(url);
     if (!response.ok)
     {
       throw new Error('Failed to fetch data.');
     }
-    const data = await response.json()
-    responseData.value = data.posts;
+    const data = await response.json();
+    responseData.value.push(...data.posts);
+    continuationToken.value = data.continuationToken ? encodeURIComponent(JSON.stringify(data.continuationToken)) : null;
   } catch (err) {
     error.value = err.message;
   } finally {
@@ -31,7 +36,7 @@ onMounted(async () => {
 
 <template>
   <div class="flex flex-col items-center pt-11 mx-10">
-    <div class="space-y-3">
+    <div class="space-y-3 mb-8">
       <FeaturedPost
         title="Breaking news: Being a genius coder who drinks a lot has been linked with 100% chance of being a gazillionaire!"
         date="20 April 1998" />
@@ -39,6 +44,7 @@ onMounted(async () => {
         <PostListItem class="my-5" :key="post.id" :title="post.title" :category="post.category" :date="post.date"
           :coverImageUrl="post.coverImageUrl" />
       </router-link>
+      <button v-if="continuationToken && !loading" class="border-solid border-2 border-black rounded-lg w-full h-12 text-lg" @click="fetchData">More Posts</button>
     </div>
   </div>
 </template>
