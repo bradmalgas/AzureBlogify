05b56c397a35b81fbfc154a5ea220e8002b7a66a
Brad Malgas <bradstermalgas@gmail.com>
2024-06-09 14:32:26 +0200

    Updated view of post list item

--- azure-blogify-ui/src/components/PostListItem.vue
+++ azure-blogify-ui/src/components/PostListItem.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
-defineProps(['title', 'date'])
+import { format } from 'date-fns';
+import { computed } from 'vue';
+const props = defineProps({
+  title: String,
+  category: String,
+  date: { type: String, required: true},
+  coverImageUrl: String
+});
+
+const formattedDate = computed(() => {
+    const date = new Date(props.date);
+    return format(date, 'dd MMMM yyyy');
+})
 </script>
 
 <template>
     <div class="flex space-x-3 lg:max-h-[200px] max-h-[80px] max-w-full">
-        <div><img src="../assets/images/placeholder-image.png" alt="" class="lg:max-w-44 max-w-36 h-auto"></div>
+        <div><img :src="coverImageUrl" class="object-cover lg:max-w-44 min-w-36 h-[80px]"></div>
         <div class="flex flex-col">
             <h2 class="lg:max-w-[600px] max-w-[510px] sm:max-sm:max-w-[200px] md:line-clamp-2 line-clamp-3 text-sm md:text-base leading-tight">{{ title }}</h2>
-            <p class="mt-auto md:text-sm text-xs">{{date}}</p>
+            <!-- <p class="text-sm">{{ category }}</p> -->
+            <p class="mt-auto md:text-sm text-xs">{{formattedDate}}</p>
         </div>
     </div>
 </template>
