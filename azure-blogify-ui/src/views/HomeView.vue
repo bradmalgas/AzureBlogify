13728cae3453f0144df7c352de0373791d7494af
Brad Malgas <bradstermalgas@gmail.com>
2024-06-09 19:14:56 +0200

    Fix/frontend fixes 09june 24 (#13)
    * Added date formatting
    
    * Update date format
    
    * Update formatting and layout
    
    * update image styling
    
    * style change for post view
    
    * featured post now working correctly
    
    * removed unecessary padding

--- azure-blogify-ui/src/views/HomeView.vue
+++ azure-blogify-ui/src/views/HomeView.vue
@@ -15,8 +15,7 @@ const fetchData = async () => {
     let url = `/api/GetPosts?pageSize=${pageSize.value}`;
     if (continuationToken.value) url += `&contToken=${continuationToken.value}`;
     const response = await fetch(url);
-    if (!response.ok)
-    {
+    if (!response.ok) {
       throw new Error('Failed to fetch data.');
     }
     const data = await response.json();
@@ -35,16 +34,20 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center pt-11 mx-10">
+  <div v-if="responseData" class="flex flex-col items-center mx-10">
     <div class="space-y-3 mb-8">
-      <FeaturedPost
-        title="Breaking news: Being a genius coder who drinks a lot has been linked with 100% chance of being a gazillionaire!"
-        date="20 April 1998" />
-      <router-link v-for="post in responseData" :key="post.id" :to="'/post/' + post.category + '/' + post.id">
+      <router-link v-if="responseData.length > 0" :to="'/post/' + responseData[0].category + '/' + responseData[0].id">
+        <FeaturedPost class="my-5" :key="responseData[0].id" :title="responseData[0].title"
+          :category="responseData[0].category" :date="responseData[0].date"
+          :coverImageUrl="responseData[0].coverImageUrl" />
+      </router-link>
+      <router-link v-for="post in responseData.slice(1)" :key="post.id" :to="'/post/' + post.category + '/' + post.id">
         <PostListItem class="my-5" :key="post.id" :title="post.title" :category="post.category" :date="post.date"
           :coverImageUrl="post.coverImageUrl" />
       </router-link>
-      <button v-if="continuationToken && !loading" class="border-solid border-2 border-black rounded-lg w-full h-12 md:text-lg text-base" @click="fetchData">More Posts</button>
+      <button v-if="continuationToken && !loading"
+        class="border-solid border-2 border-black rounded-lg w-full h-12 md:text-lg text-base" @click="fetchData">More
+        Posts</button>
     </div>
   </div>
 </template>
--- azure-blogify-ui/src/views/PostItemView.vue
+++ azure-blogify-ui/src/views/PostItemView.vue
@@ -3,6 +3,7 @@ import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import markdownit from 'markdown-it'
 import NotFoundView from './NotFoundView.vue';
+import { formatDate } from 'date-fns';
 
 const route = useRoute();
 
@@ -22,44 +23,48 @@ async function fetchData(params) {
     const data = await response.json();
     postContent.value = md.render(data.content);
     postItem.value = data;
+    postItem.value.date = await stringToDate(data.date);
   } catch (err) {
     error.value = err.toString();
   } finally {
     loading.value = false;
   }
 };
+
+async function stringToDate(date) {
+  return formatDate(date, "dd MMM, yyyy");
+}
 </script>
 
 
 <template>
-  <div class="flex flex-col items-center pt-11 mx-10">
+  <div class="flex flex-col items-center mx-10">
     <div v-if="loading">
       <p class="md:text-xl">Loading ...</p>
     </div>
     <div v-if="error">
       <NotFoundView />
     </div>
-    <div v-if="postItem" class="flex justify-center my-5">
-      <div class="flex flex-col items-center pt-5 mx-10 max-w-[1024px]">
-        <div>
-          <img class="max-w-full h-auto" src="../assets/images/placeholder-image.png" alt="">
-          <h1
-            class="flex lg:max-w-[600px] max-w-[510px] sm:max-sm:max-w-[200px] md:text-3xl text-xl my-1 font-bold leading-tight">
+    <div v-if="postItem" class="flex justify-center my-3">
+      <div class="flex flex-col items-center pt-5 max-w-[1024px]">
+        <div class="w-full">
+          <h1 class="flex lg:max-w-[600px] max-w-[510px] sm:max-sm:max-w-[200px] md:text-5xl text-3xl my-1 font-bold">
             {{ postItem.title }}
           </h1>
-          <div class="flex my-3 space-x-4">
-            <div class="">
-              <img class="object-cover object-top rounded-full h-[100px] w-[100px] mb-3 border border-neutral-900"
+          <div class="flex space-x-4 my-5">
+            <div>
+              <img class="object-cover object-top rounded-full h-[55px] w-[55px] border border-neutral-900"
                 src="../assets/images/user-placeholder-image.png" alt="">
             </div>
             <div class="flex flex-col justify-center">
-              <p class="md:text-lg text-sm font-semibold">{{ postItem.author }}</p>
-              <p class="md:text-sm text-xs">{{ postItem.date }}</p>
+              <p class="md:text-lg text-base">{{ postItem.author }}</p>
+              <p class="md:text- text-sm font-light">{{ postItem.date }}</p>
             </div>
           </div>
         </div>
-        <div>
-          <article class="prose prose-sm my-3" v-html="postContent">
+        <img class="my-3 h-96" :src="postItem.coverImageUrl" alt="">
+        <div class="mt-5">
+          <article class="prose prose-sm lg:prose-lg my-3" v-html="postContent">
           </article>
         </div>
       </div>
