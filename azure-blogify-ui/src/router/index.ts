50decfe8d9ca33162d1f77b55d49b712e46bf99e
Brad Malgas <bradstermalgas@gmail.com>
2024-06-11 23:24:31 +0200

    Remove fallback title

--- azure-blogify-ui/src/router/index.ts
+++ azure-blogify-ui/src/router/index.ts
@@ -31,7 +31,7 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = (to.meta.title as string) || 'AzureBlogify';
+  document.title = (to.meta.title as string);
   next();
 });
 
