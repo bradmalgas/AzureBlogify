efe5eaa1505353c47fc9b509f63f46b92f4cf1b0
Brad Malgas <bradstermalgas@gmail.com>
2024-06-11 23:24:50 +0200

    Added unexpected error page

---
+++ azure-blogify-ui/src/views/UnexpectedErrorView.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+  <div class="bg-white p-8 rounded-lg text-center">
+    <h1 class="text-4xl font-bold mb-4">Oops!</h1>
+    <p class="text-gray-700 text-lg">Something went wrong. Please try again later.</p>
+    <a href="/"><button class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+      Go Back
+    </button></a>
+  </div>
+</div>
+</template>
