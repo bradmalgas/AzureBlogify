5cb0b4630d9999ccc9e98f6b72f17edddbe1f4f9
Brad Malgas <bradstermalgas@gmail.com>
2024-06-11 23:23:57 +0200

    Added bold titles

--- azure-blogify-ui/src/components/PostListItem.vue
+++ azure-blogify-ui/src/components/PostListItem.vue
@@ -18,7 +18,7 @@ const formattedDate = computed(() => {
     <div class="flex space-x-3 lg:max-h-[200px] max-h-[80px] max-w-full">
         <div><img :src="coverImageUrl" class="object-cover lg:max-w-44 min-w-36 h-[80px]"></div>
         <div class="flex flex-col">
-            <h2 class="lg:max-w-[600px] max-w-[510px] sm:max-sm:max-w-[200px] md:line-clamp-2 line-clamp-3 text-sm md:text-base leading-tight">{{ title }}</h2>
+            <h2 class="lg:max-w-[600px] max-w-[510px] sm:max-sm:max-w-[200px] md:line-clamp-2 line-clamp-3 text-sm md:text-base leading-tight font-bold">{{ title }}</h2>
             <!-- <p class="text-sm">{{ category }}</p> -->
             <p class="mt-auto md:text-sm text-xs">{{formattedDate}}</p>
         </div>
