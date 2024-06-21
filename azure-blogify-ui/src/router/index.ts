20f686d3f8b28d41897e33c46a6b0f2a21dc7897
Brad Malgas <bradstermalgas@gmail.com>
2024-06-21 21:02:45 +0200

    Temp loading title

--- azure-blogify-ui/src/router/index.ts
+++ azure-blogify-ui/src/router/index.ts
@@ -31,7 +31,7 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = (to.meta.title as string);
+  document.title = (to.meta.title as string) || "Loading... | AzureBlogify";
   next();
 });
 
