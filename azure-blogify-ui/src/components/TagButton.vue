258849b4b61387ca8a866a5950690784e960a696
Brad Malgas <bradstermalgas@gmail.com>
2024-06-23 21:30:52 +0200

    Added calendar icon Added button for tags

---
+++ azure-blogify-ui/src/components/TagButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps({
+    text: { type: String, required: true },
+});
+</script>
+
+<template>
+    <button class="bg-gray-100 py-1 px-4 rounded-full uppercase text-[8px] outline outline-1 outline-gray-800">{{ props.text }}</button>
+</template>
