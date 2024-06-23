3ad96372cf3264499a5bd3f41e986e1b9e6c9f26
Brad Malgas <bradstermalgas@gmail.com>
2024-06-24 00:38:29 +0200

    Complete redesign of footer

--- azure-blogify-ui/src/components/FooterInfo.vue
+++ azure-blogify-ui/src/components/FooterInfo.vue
@@ -5,15 +5,30 @@ import TwitterIcon from './icons/TwitterIcon.vue';
 </script>
 
 <template>
-    <div class="flex flex-col h-[120px] bg-babyblue items-center py-5 space-y-3">
-        <p class="text-xs font-light">Copyright © 2024 Brad Malgas</p>
-        <div class="flex space-x-5">
-            <a href="https://twitter.com/_brvd_" target="_blank"><TwitterIcon class="w-7"/></a>
-            <a href="https://github.com/bradmalgas" target="_blank"><GitHubIcon class="w-7"/></a>
-            <a href="https://www.linkedin.com/in/brad-malgas/" target="_blank"><LinkedInLogo class="w-7"/></a>
+    <div class="flex flex-col h-[250px] bg-black text-white items-center py-6 font-serif">
+        <div class="flex flex-col items-center">
+            <p class="text-xs">Subscribe to our newsletter</p>
+            <div class="flex mt-7 rounded-full outline outline-2 outline-white">
+                <textarea class="w-52 rounded-full h-full bg-black text-xs pl-5 flex content-center" name="emailTextField"
+                    id="newsletter-email-address-text-field">Enter your email</textarea>
+                <button class="text-xs bg-white text-black font-bold rounded-full py-2 px-6 h-full">Subscribe</button>
+            </div>
+        </div>
+        <div class="flex flex-col mt-auto w-full items-center">
+            <p class="text-xs font-thin">© 2024 Brad Malgas</p>
+            <div class="flex space-x-3">
+                <a href="https://twitter.com/_brvd_" target="_blank">
+                    <TwitterIcon colour="#ffffff" class="w-8" />
+                </a>
+                <a href="https://github.com/bradmalgas" target="_blank">
+                    <GitHubIcon colour="#ffffff" class="w-8" />
+                </a>
+                <a href="https://www.linkedin.com/in/brad-malgas/" target="_blank">
+                    <LinkedInLogo colour="#ffffff" class="w-8" />
+                </a>
+            </div>
         </div>
     </div>
 </template>
 
-<style lang="postcss" scoped>
-</style>
+<style lang="postcss" scoped></style>
