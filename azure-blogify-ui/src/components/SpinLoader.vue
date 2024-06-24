25ca6078aca569c7fc7c39a1a3568f6b416d5fd6
Brad Malgas <bradstermalgas@gmail.com>
2024-06-24 17:14:35 +0200

    Created my own spin loader!

---
+++ azure-blogify-ui/src/components/SpinLoader.vue
@@ -0,0 +1,44 @@
+<template>
+      <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+    <circle class="spin2" cx="400" cy="400" fill="none"
+      r="200" stroke-width="50" :stroke="colour"
+      stroke-dasharray="700 1400"
+      stroke-linecap="round" />
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+    colour: {type: String, required: true}
+})
+</script>
+
+<style>
+    @keyframes spin {
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  
+    @keyframes spin2 {
+      0% {
+        stroke-dasharray: 1, 800;
+        stroke-dashoffset: 0;
+      }
+      50% {
+        stroke-dasharray: 400, 400;
+        stroke-dashoffset: -200px;
+      }
+      100% {
+        stroke-dasharray: 800, 1;
+        stroke-dashoffset: -800px;
+      }
+    }
+  
+    .spin2 {
+      transform-origin: center;
+      animation: spin2 2s ease-in-out infinite,
+        spin 4.5s linear infinite;
+      animation-direction: alternate;
+    }
+</style>
