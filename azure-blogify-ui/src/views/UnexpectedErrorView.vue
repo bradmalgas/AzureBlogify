fc90da89de805ca9075d4dabd82218a8b798f0d6
Brad Malgas <bradstermalgas@gmail.com>
2024-06-24 18:05:25 +0200

    Added slightly tweaked unexpected error page

--- azure-blogify-ui/src/views/UnexpectedErrorView.vue
+++ azure-blogify-ui/src/views/UnexpectedErrorView.vue
@@ -1,13 +1,16 @@
-<script setup></script>
+<script setup>
+import ShrugIcon from '@/components/icons/ShrugIcon.vue'
+</script>
 
 <template>
-    <div class="flex items-center justify-center min-h-screen">
-  <div class="bg-white p-8 rounded-lg text-center">
-    <h1 class="text-4xl font-bold mb-4">Oops!</h1>
-    <p class="text-gray-700 text-lg">Something went wrong. Please try again later.</p>
-    <a href="/"><button class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
-      Go Back
-    </button></a>
+  <div class="flex flex-col items-center justify-center min-h-svh">
+    <ShrugIcon colour="#000000" class="h-44 w-44"/>
+    <div class="bg-white p-8 rounded-lg text-center">
+      <h1 class="text-4xl font-bold mb-4">Oops!</h1>
+      <p class="text-gray-700 text-lg">Something went wrong. Please try again later.</p>
+      <a href="/"><button class="mt-6 px-6 py-2 bg-black text-white rounded-3xl hover:bg-gray-600 focus:outline-none">
+          Go Back
+        </button></a>
+    </div>
   </div>
-</div>
 </template>
