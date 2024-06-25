21cedd6eeaba41bdb72bd84e66ba7634bcdd7776
Brad Malgas <bradstermalgas@gmail.com>
2024-06-26 00:36:21 +0200

    Updated styling on about ivew

--- azure-blogify-ui/src/views/AboutView.vue
+++ azure-blogify-ui/src/views/AboutView.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
 import AboutMe from '@/components/AboutMe.vue';
+import { onMounted } from 'vue';
 
+onMounted(async () => {
+  window.scrollTo(0, 0);
+})
 </script>
 
 <template>
     <div class="flex justify-center">
-  <div class="flex flex-col items-center pt-11 px-10 md:max-w-[768px] lg:max-w-[1024px]">
+  <div class="flex flex-col items-center pt-5 px-5 md:max-w-[768px] lg:max-w-[1024px]">
       <AboutMe/>
-      <div class="mt-6 space-y-3">
-      <p>Spider-Man's secret identity is Brad Benjamin Malgas. Initially, Brad was depicted as a teenage high-school student and an orphan raised by his Aunt May and Uncle Ben in New York City after his parents Richard and Mary Malgas died in a plane crash.</p>
-      <p>Lee and Ditko had the character deal with the struggles of adolescence and financial issues and gave him many supporting characters, such as Flash Thompson, J. Jonah Jameson, and Harry Osborn; romantic interests Gwen Stacy, Mary Jane Watson, and the Black Cat; and enemies such as the Green Goblin, Doctor Octopus, and Venom.</p>
-      <p>In his origin story, Spider-Man gets his superhuman spider-powers and abilities after being bitten by a radioactive spider. These powers include superhuman strength, agility, reflexes, stamina, durability, coordination, and balance; clinging to surfaces and ceilings like a spider; and detecting danger with his precognition ability called "spider-sense".</p>
-      <p>He builds wrist-mounted "web-shooter" devices that shoot artificial spider-webs of his own design, which he uses both for fighting and for web-swinging across the city. Brad Malgas originally used his powers for his own personal gain, but after his Uncle Ben was killed by a thief that Brad could not stop, he began to use his powers to fight crime by becoming Spider-Man.</p>
+      <div class="my-6 space-y-3">
+      <p class="text-sm">Spider-Man's secret identity is Brad Benjamin Malgas. Initially, Brad was depicted as a teenage high-school student and an orphan raised by his Aunt May and Uncle Ben in New York City after his parents Richard and Mary Malgas died in a plane crash.</p>
+      <p class="text-sm">Lee and Ditko had the character deal with the struggles of adolescence and financial issues and gave him many supporting characters, such as Flash Thompson, J. Jonah Jameson, and Harry Osborn; romantic interests Gwen Stacy, Mary Jane Watson, and the Black Cat; and enemies such as the Green Goblin, Doctor Octopus, and Venom.</p>
+      <p class="text-sm">In his origin story, Spider-Man gets his superhuman spider-powers and abilities after being bitten by a radioactive spider. These powers include superhuman strength, agility, reflexes, stamina, durability, coordination, and balance; clinging to surfaces and ceilings like a spider; and detecting danger with his precognition ability called "spider-sense".</p>
+      <p class="text-sm">He builds wrist-mounted "web-shooter" devices that shoot artificial spider-webs of his own design, which he uses both for fighting and for web-swinging across the city. Brad Malgas originally used his powers for his own personal gain, but after his Uncle Ben was killed by a thief that Brad could not stop, he began to use his powers to fight crime by becoming Spider-Man.</p>
       </div>
     </div>
 </div>
 </template>
 
-<style> 
+<style>
 </style>
