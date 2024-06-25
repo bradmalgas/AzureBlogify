c12512ac128a744d7a383576f634831a64226517
Brad Malgas <bradstermalgas@gmail.com>
2024-06-25 02:15:25 +0200

    Replace "AuzreBlogify" with my own logo

--- azure-blogify-ui/src/components/NavBar.vue
+++ azure-blogify-ui/src/components/NavBar.vue
@@ -2,6 +2,7 @@
 import LogoSmall from './icons/LogoSmall.vue';
 import SearchIcon from './icons/SearchIcon.vue';
 import SideNav from './SideNav.vue';
+import BradLogo from './icons/BradLogo.vue';
 </script>
 
 <template>
@@ -20,7 +21,7 @@ import SideNav from './SideNav.vue';
         </div>
         <div class="hidden lg:flex space-x-6 ml-auto mr-20">
             <router-link to="/" key="home">Home</router-link>
-            <router-link to="/about" key="about">About</router-link>    
+            <router-link to="/about" key="about">About</router-link>
         </div>
         <div class=" flex bg-gray-200 rounded-[13px] p-2 mr-3">
             <input class="bg-gray-200 rounded-[13px] min-w-52" type="text" placeholder="Search..">
@@ -31,7 +32,9 @@ import SideNav from './SideNav.vue';
     <!-- Mobile NavBar -->
     <div class="flex justify-between items-center bg-white overflow-hidden md:hidden min-w-80 px-5 py-7 max-h-[52px] border-0 border-black border-b">
         <div class="flex items-center relative">
-            <router-link to="/" key="home"><p class="text-3xl font-semibold font-serif">AzureBlogify</p></router-link>
+            <router-link to="/" key="home">
+                <BradLogo/>
+            </router-link>
         </div>
         <div class="px-3">
             <SideNav />
