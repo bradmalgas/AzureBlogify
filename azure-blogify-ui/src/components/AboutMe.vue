2a7296c6cb2a0eacb5765bf5f06624d41547023d
Brad Malgas <bradstermalgas@gmail.com>
2024-06-26 00:34:28 +0200

    redesign about me page

--- azure-blogify-ui/src/components/AboutMe.vue
+++ azure-blogify-ui/src/components/AboutMe.vue
@@ -7,17 +7,17 @@ import TwitterIcon from './icons/TwitterIcon.vue';
 
 <template>
     <div
-        class="md:mb-6 flex flex-col items-center text-center space-y-1 lg:max-w-[800px] max-w-[510px] sm:max-sm:max-w-[200px] min-w-[150px]">
-        <img class="object-cover object-top rounded-full h-[150px] w-[150px] mb-3 border-4 border-neutral-900"
-            src="https://storageazureblogify.blob.core.windows.net/blogimages/user-placeholder-image.png" alt="">
-        <h1 class="md:text-3xl text-2xl font-bold my-2">Brad Malgas</h1>
-        <p>Intermediate .NET Developer & Azure Cloud Enthusiast | 4 x Microsoft Certified | Experienced in Cloud-Native
-            Solutions | Football Lover</p>
+        class="md:mb-6 flex flex-col items-center text-center space-y-3 lg:max-w-[800px] max-w-[510px] sm:max-sm:max-w-[200px] min-w-[150px]">
+        <img class="object-cover object-top rounded-full h-[100px] w-[100px] border mt-3"
+            src="https://storageazureblogify.blob.core.windows.net/blogimages/IMG-4128.jpeg" alt="">
+        <h1 class="md:text-3xl text-3xl font-semibold my-2 font-serif uppercase">Brad Malgas</h1>
+        <p class="text-[15px] leading-snug text-gray-600">Intermediate .NET Developer & Azure Cloud Engineer | 4 x Microsoft Certified | Experienced in Cloud-Native
+            Solutions</p>
 
         <div class="flex space-x-3">
-            <a href="https://twitter.com/_brvd_" target="_blank"><TwitterIcon class="w-8"/></a>
-            <a href="https://github.com/bradmalgas" target="_blank"><GitHubIcon class="w-8"/></a>
-            <a href="https://www.linkedin.com/in/brad-malgas/" target="_blank"><LinkedInLogo class="w-8"/></a>
+            <a href="https://twitter.com/_brvd_" target="_blank"><TwitterIcon colour="#4e4e4e" class="w-6 h-6"/></a>
+            <a href="https://github.com/bradmalgas" target="_blank"><GitHubIcon colour="#4e4e4e"  class="w-6 h-6"/></a>
+            <a href="https://www.linkedin.com/in/brad-malgas/" target="_blank"><LinkedInLogo colour="#4e4e4e"  class="w-6 h-6"/></a>
         </div>
     </div>
 </template>
