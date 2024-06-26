077f955f1135ef146037e14bfb059c029490749c
Brad Malgas <bradstermalgas@gmail.com>
2024-06-27 01:06:46 +0200

    Redesigned navbar for tablet/laptop

--- azure-blogify-ui/src/components/NavBar.vue
+++ azure-blogify-ui/src/components/NavBar.vue
@@ -7,36 +7,40 @@ import BradLogo from './icons/BradLogo.vue';
 
 <template>
     <!-- Tablet/Laptop NavBar -->
-    <div class="bg-babyblue truncate hidden md:flex justify-between items-center py-1 lg:sticky top-0">
+    <div
+        class="bg-white border-0 border-black border-b truncate hidden md:flex justify-between items-center max-h-[100px] lg:sticky top-0">
         <div class="px-3">
             <SideNav />
         </div>
-        <div class="lg:pl-5">
+        <div>
             <router-link to="/" class="">
-                <div class="flex items-center">
-                    <LogoSmall class="max-h-[52px] h-full w-full object-cover" />
-                    <h1 class="lg:text-xl text-xl font-semibold">AzureBlogify</h1>
-                </div>
+                <BradLogo />
             </router-link>
         </div>
-        <div class="hidden lg:flex space-x-6 ml-auto mr-20">
-            <router-link to="/" key="home">Home</router-link>
-            <router-link to="/about" key="about">About</router-link>
+        <div class="hidden lg:flex items-center">
+            <div class="space-x-6 mr-6">
+                <router-link to="/" key="home">Home</router-link>
+                <router-link to="/about" key="about">About</router-link>
+            </div>
+            <div class="flex bg-gray-200 rounded-[13px] p-2 mr-3">
+                <input class="bg-gray-200 rounded-[13px] min-w-52" type="text" placeholder="Search..">
+                <SearchIcon class="h-6 pr-2" />
+            </div>
         </div>
-        <div class=" flex bg-gray-200 rounded-[13px] p-2 mr-3">
-            <input class="bg-gray-200 rounded-[13px] min-w-52" type="text" placeholder="Search..">
-            <SearchIcon class="h-6 pr-2" />
+        <div class="lg:hidden">
+            <SearchIcon class="h-8 mr-6" />
         </div>
     </div>
 
     <!-- Mobile NavBar -->
-    <div class="flex justify-between items-center bg-white overflow-hidden md:hidden min-w-80 px-5 py-7 max-h-[52px] border-0 border-black border-b">        
+    <div
+        class="flex justify-between items-center bg-white overflow-hidden md:hidden min-w-80 px-5 max-h-[52px] border-0 border-black border-b">
         <div>
             <SideNav />
         </div>
         <div class="flex items-center relative">
             <router-link to="/" key="home">
-                <BradLogo/>
+                <BradLogo />
             </router-link>
         </div>
         <div>
@@ -45,5 +49,4 @@ import BradLogo from './icons/BradLogo.vue';
     </div>
 </template>
 
-<style lang="postcss" scoped>
-</style>
+<style lang="postcss" scoped></style>
