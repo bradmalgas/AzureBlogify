01c03bdf821f23d8851a0341a2b84f9e1f6e5b45
Brad Malgas <bradstermalgas@gmail.com>
2024-06-27 22:26:02 +0200

    Updated z-indexes: - 10: background - 20: Sidemenu
    Added correct mouse on hover

--- azure-blogify-ui/src/components/SideNav.vue
+++ azure-blogify-ui/src/components/SideNav.vue
@@ -13,22 +13,22 @@ const toggleSideNav = () => {
 
 <template>
     <div>
-      <div class="left-8 relative hover:cursor-pointer" :class="{'slide-out z-30': !isOpen, 'slide-in z-0': isOpen}">
-        <MenuIcon class="h-full" @click="toggleSideNav()" />
+      <div>
+        <MenuIcon class="h-full hover:cursor-pointer" @click="toggleSideNav()" />
       </div>
-      <div class="fixed inset-0 flex z-10">
-        <div id="slide-nav-component" class="bg-black text-white max-sm:min-w-[80%] w-full md:max-w-md h-screen p-5 pt-8 relative z-40" :class="{'slide-out': !isOpen, 'slide-in': isOpen}">
+      <div class="fixed inset-0 z-20" :class="{'slide-out': !isOpen, 'slide-in': isOpen}">
+        <div id="slide-nav-component" class="bg-black text-white max-sm:max-w-[60%] w-full md:max-w-md h-screen p-5 pt-8 relative">
           <div class="absolute top-3 right-8">
-            <CloseIcon colour="#ffffff" class="h-full" @click="toggleSideNav()" />
+            <CloseIcon colour="#ffffff" class="h-full hover:cursor-pointer" @click="toggleSideNav()" />
           </div>
-          <div class="grid gap-4 text-xl md:text-2xl lg:text-2xl rounded ml-5">
+          <div class="grid gap-4 font-bold text-xl md:text-2xl lg:text-2xl rounded ml-5">
             <BradLogo class="border-0 border-b border-white" colour="#ffffff"/>
             <router-link class="hover:cursor-pointer max-w-fit" to="/" key="home" @click="toggleSideNav()">Home</router-link>
             <router-link class="hover:cursor-pointer max-w-fit" to="/about" key="about" @click="toggleSideNav()">About</router-link>
           </div>
         </div>
       </div>
-        <div class="bg-black opacity-30 inset-0 fixed flex" :class="{'slide-out hidden': !isOpen, 'slide-in flex': isOpen}" @click="toggleSideNav()"></div>
+      <div class="bg-black opacity-30 inset-0 fixed z-10" :class="{'fade-out': !isOpen, 'fade-in': isOpen}" @click="toggleSideNav()"></div>
     </div>
   </template>
   
@@ -43,5 +43,16 @@ const toggleSideNav = () => {
 .slide-in {
   transform: translateX(0);
   transition: transform 0.4s ease;
+}
+
+.fade-in {
+  opacity: 0.3;
+  transition: opacity 0.5s ease;
+}
+
+.fade-out {
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.4s ease, visibility 0.4s ease;
 }
 </style>
