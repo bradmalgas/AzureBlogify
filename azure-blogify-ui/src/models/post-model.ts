2e457f499376a1a057de1bbd5ce46e4cfedc1c40
Brad Malgas <bradstermalgas@gmail.com>
2024-07-03 00:14:25 +0200

    Added post model to FE project

---
+++ azure-blogify-ui/src/models/post-model.ts
@@ -0,0 +1,13 @@
+export interface Post {
+    id: string;
+    title: string;
+    author: string;
+    category: string;
+    date: string;
+    content: string;
+    summary: string;
+    tags: string;
+    coverImageUrl: string;
+    readingMinutes: string;
+    disclaimer: string;
+  }
