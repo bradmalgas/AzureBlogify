a1ff41df92418709064b9efa05b9b7d759b82312
Brad Malgas <bradstermalgas@gmail.com>
2024-07-05 01:18:55 +0200

    Added backend function to create new post

---
+++ azure-blogify-ui/api/CreatePost.cs
@@ -0,0 +1,75 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos;
+
+namespace api
+{
+    public static class CreatePost
+    {
+        [FunctionName("CreatePost")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody;
+            try
+            {
+                 requestBody = await new StreamReader(req.Body).ReadToEndAsync();                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Could not read the request body: {req.Body.ToString}. Error: {ex}");
+            }
+
+            Post data;
+            try
+            {
+                data = JsonConvert.DeserializeObject<Post>(requestBody);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Could not parse the JSON: {requestBody}. Error: {ex}");
+            }
+
+            log.LogInformation($"HTTP trigger: Create Post. [{DateTime.Now}]");
+
+            CosmosClient client = new(
+                connectionString: Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING")!
+            );
+
+            Database db = client.GetDatabase("azure-blogify-cosmosdb");
+            Container blogPosts = db.GetContainer("blogPosts");
+
+            data.Id = Guid.NewGuid();
+            data.ReadingMinutes = calculateReadingTime(data.Content);
+
+            if (data.IsValid)
+            {
+                Post createdItem = await blogPosts.CreateItemAsync(data, partitionKey: new PartitionKey(data.Category));
+                return new OkObjectResult(createdItem);
+            }
+            else {
+                return new BadRequestObjectResult("Could not post data - missing fields");
+                };
+        }
+
+        public static int calculateReadingTime(string postContent)
+        {
+            if (string.IsNullOrWhiteSpace(postContent))
+            {
+                return 0;
+            }
+
+            string[] words = postContent.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return Math.Max((int)Math.Round((double)words.Length / 150, 0), 1);
+        }
+    }
+}
