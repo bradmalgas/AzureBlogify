e7dba831066d9d80ae4528cb284989261b8427cc
Brad Malgas <bradstermalgas@gmail.com>
2024-07-11 00:32:19 +0200

    Created delete post

---
+++ azure-blogify-ui/api/DeletePost.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos;
+using System.Web;
+
+namespace api
+{
+    public static class DeletePost
+    {
+        [FunctionName("DeletePost")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string id = req.Query["id"];
+            string category = req.Query["category"];
+
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(category)) return new BadRequestObjectResult("Could not delete post - missing fields");
+
+            string partitionKey = HttpUtility.UrlDecode(category);
+
+            log.LogInformation($"HTTP trigger: Delete By Id. [{DateTime.Now}]");
+
+            CosmosClient client = new(
+                connectionString: Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING")!
+            );
+
+            Database db = client.GetDatabase("azure-blogify-cosmosdb");
+            Container blogPosts = db.GetContainer("blogPosts");
+
+            ItemResponse<Post> readItemResponse;
+            try
+            {
+                readItemResponse = await blogPosts.DeleteItemAsync<Post>(
+                    id: id,
+                    partitionKey: new PartitionKey(partitionKey)
+                );
+
+                Post responseMessage = readItemResponse.Resource;
+
+                return new OkObjectResult(responseMessage);
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound) { }
+            {
+                return new BadRequestObjectResult("Could not delete post - item does not exist");
+            }
+        }
+    }
+}
