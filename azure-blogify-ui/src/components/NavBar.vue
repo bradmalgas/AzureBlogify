e35abcb13fa419816a195c37237ecf6e4c221b6d
Brad Malgas <bradstermalgas@gmail.com>
2024-12-02 22:26:16 +0200

    Switcherooooooooo

--- azure-blogify-ui/src/components/NavBar.vue
+++ azure-blogify-ui/src/components/NavBar.vue
@@ -15,6 +15,28 @@ const { showSearchBar, searchString } = storeToRefs(navbar);
 </script>
 
 <template>
+    <!-- Mobile/Tablet NavBar -->
+    <div
+        class="flex justify-between items-center bg-white overflow-hidden lg:hidden min-w-80 max-h-[60px] border-0 border-black border-b sticky top-0 z-10 px-3">
+        <div>
+            <SideNav />
+        </div>
+        <div class="flex items-center relative">
+            <router-link to="/" key="home">
+                <BradLogo />
+            </router-link>
+        </div>
+        <div class="flex">
+            <input v-show="showSearchBar" v-model="searchString"
+                class="bg-gray-200 rounded-[13px] min-w-52 focus:outline-none pl-2" type="text" placeholder="Search.."
+                @keyup.enter="navbar.search">
+            <SearchIcon @click="navbar.toggleSearchBar" v-show="!showSearchBar"
+                class="h-8 mx-2 pr-2 hover:cursor-pointer" />
+            <CloseIcon @click="navbar.toggleSearchBar" v-show="showSearchBar"
+                class="h-6 mx-2 px-2 hover:cursor-pointer" />
+        </div>
+    </div>
+
     <!-- Laptop NavBar -->
     <div
         class="bg-white border-0 border-black border-b truncate hidden lg:flex justify-between items-center max-h-[80px] sticky top-0 z-10 px-3">
@@ -40,28 +62,6 @@ const { showSearchBar, searchString } = storeToRefs(navbar);
                 <SearchIcon @click="navbar.search" class="h-6 pr-2 hover:cursor-pointer" />
             </div>
         </div>
-    </div>
-
-    <!-- Mobile/Tablet NavBar -->
-    <div
-        class="flex justify-between items-center bg-white overflow-hidden lg:hidden min-w-80 max-h-[60px] border-0 border-black border-b sticky top-0 z-10 px-3">
-        <!-- <div>
-            <SideNav />
-        </div>
-        <div class="flex items-center relative">
-            <router-link to="/" key="home">
-                <BradLogo />
-            </router-link>
-        </div>
-        <div class="flex">
-            <input v-show="showSearchBar" v-model="searchString"
-                class="bg-gray-200 rounded-[13px] min-w-52 focus:outline-none pl-2" type="text" placeholder="Search.."
-                @keyup.enter="navbar.search">
-            <div @click="navbar.toggleSearchBar" v-show="!showSearchBar" class="h-8 mx-2 pr-2 hover:cursor-pointer">Search</div>
-                <div @click="navbar.toggleSearchBar" v-show="showSearchBar" class="h-6 mx-2 px-2 hover:cursor-pointer">Close</div>
-        </div> -->
-        <div @click="navbar.toggleSearchBar" v-show="!showSearchBar">Search</div>
-        <div @click="navbar.toggleSearchBar" v-show="showSearchBar">Close</div>
     </div>
 </template>
 
