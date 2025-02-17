6dc306185e0012e6a244dcb6bd86383bf98ec206
Brad Malgas <bradstermalgas@gmail.com>
2025-02-17 14:20:47 +0200

    Refactor user store

--- azure-blogify-ui/src/stores/user.ts
+++ azure-blogify-ui/src/stores/user.ts
@@ -5,11 +5,15 @@ import { computed, ref } from 'vue'
 export const useUserStore = defineStore('user', () => {
   const user = ref({} as UserModel)
   const isLoggedIn = computed(() => {
-    if (!isEmpty()) {return user.value.userRoles.includes('authenticated')}
+    if (!isEmpty()) {
+      return user.value.userRoles.includes('authenticated')
+    }
     return false
   })
   const isAdmin = computed(() => {
-    if (!isEmpty()) {return user.value.userRoles.includes('administrator')}
+    if (!isEmpty()) {
+      return user.value.userRoles.includes('administrator')
+    }
     return false
   })
 
@@ -17,10 +21,22 @@ export const useUserStore = defineStore('user', () => {
     const response = await fetch('/.auth/me')
     const payload = await response.json()
     user.value = payload.clientPrincipal
+    try {
+      const response = await fetch('/.auth/me')
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+
+      const payload = await response.json()
+      if (payload?.clientPrincipal != null) user.value = payload.clientPrincipal
+    } catch (error: any) {
+      console.error('Error occured:', error.message)
+    }
   }
 
   function isEmpty() {
-    return Object.keys(user.value).length == 0
+    return Object.keys(user?.value)?.length == 0
   }
 
   return {
