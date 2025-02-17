67ff2182a0e1714aa85724378665acbe86aa061c
Brad Malgas <bradstermalgas@gmail.com>
2025-02-17 14:22:45 +0200

    Moved search logic to navbar store

--- azure-blogify-ui/src/components/NavBar.vue
+++ azure-blogify-ui/src/components/NavBar.vue
@@ -8,6 +8,7 @@ import { useRouter } from 'vue-router';
 import CloseIcon from './icons/CloseIcon.vue';
 import { useUserStore } from '@/stores/user';
 import { storeToRefs } from 'pinia';
+import { useNavBarStore } from '@/stores/navbar';
 
 const searchString = ref("");
 const router = useRouter();
@@ -15,18 +16,32 @@ const showSearchBar = ref(false);
 
 const store = useUserStore();
 const { isAdmin, isLoggedIn } = storeToRefs(store);
-
-const search = () => {
-    var searchquery = searchString.value.trim();
-    if (searchquery !== "") router.push(`/search/${searchquery}`);
-};
-
-const toggleSearchBar = () => {
-    showSearchBar.value = !showSearchBar.value;
-};
+const navbar = useNavBarStore();
 </script>
 
 <template>
+    <!-- Mobile/Tablet NavBar -->
+    <div
+        class="flex justify-between items-center bg-white overflow-hidden lg:hidden min-w-80 max-h-[60px] border-0 border-black border-b sticky top-0 z-10 px-3">
+        <div>
+            <SideNav />
+        </div>
+        <div class="flex items-center relative">
+            <router-link to="/" key="home">
+                <BradLogo colour="#000000" />
+            </router-link>
+        </div>
+        <div class="flex">
+            <input :style="{ display: showSearchBar ? 'block' : 'none' }" v-model="searchString"
+                class="bg-gray-200 rounded-[13px] min-w-52 focus:outline-none pl-2" type="text" placeholder="Search.."
+                @keyup.enter="navbar.search">
+            <SearchIcon @click="navbar.toggleSearchBar" :style="{ display: !showSearchBar ? 'block' : 'none' }"
+                class="h-8 mx-2 pr-2 hover:cursor-pointer" />
+            <CloseIcon colour="#000000" @click="navbar.toggleSearchBar"
+                :style="{ display: showSearchBar ? 'block' : 'none' }" class="h-6 mx-2 px-2 hover:cursor-pointer" />
+        </div>
+    </div>
+
     <!-- Laptop NavBar -->
     <div
         class="bg-white border-0 border-black border-b truncate hidden lg:flex justify-between items-center max-h-[80px] sticky top-0 z-10 px-3">
@@ -35,7 +50,7 @@ const toggleSearchBar = () => {
         </div>
         <div>
             <router-link to="/">
-                <BradLogo />
+                <BradLogo colour="#000000" />
             </router-link>
         </div>
         <div class="flex items-center">
@@ -48,29 +63,9 @@ const toggleSearchBar = () => {
             </div>
             <div class="flex bg-gray-200 rounded-[13px] p-2 mr-3">
                 <input v-model="searchString" class="bg-gray-200 rounded-[13px] min-w-52 focus:outline-none pl-2"
-                    type="text" placeholder="Search.." @keyup.enter="search">
-                <SearchIcon @click="search" class="h-6 pr-2 hover:cursor-pointer" />
+                    type="text" placeholder="Search.." @keyup.enter="navbar.search">
+                <SearchIcon @click="navbar.search" class="h-6 pr-2 hover:cursor-pointer" />
             </div>
-        </div>
-    </div>
-
-    <!-- Mobile/Tablet NavBar -->
-    <div
-        class="flex justify-between items-center bg-white overflow-hidden lg:hidden min-w-80 max-h-[60px] border-0 border-black border-b sticky top-0 z-10 px-3">
-        <div>
-            <SideNav />
-        </div>
-        <div class="flex items-center relative">
-            <router-link to="/" key="home">
-                <BradLogo />
-            </router-link>
-        </div>
-        <div class="flex">
-            <input v-if="showSearchBar" v-model="searchString"
-                class="bg-gray-200 rounded-[13px] min-w-52 focus:outline-none pl-2" type="text" placeholder="Search.."
-                @keyup.enter="search">
-            <SearchIcon v-if="!showSearchBar" @click="toggleSearchBar()" class="h-8 pr-2" />
-            <CloseIcon v-if="showSearchBar" @click="toggleSearchBar()" class="h-6 px-2" />
         </div>
     </div>
 </template>
