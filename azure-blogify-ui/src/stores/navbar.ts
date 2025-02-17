701fce7df14c13392fdc160ac95b57fc3e1b8c3a
Brad Malgas <bradstermalgas@gmail.com>
2025-02-17 14:21:40 +0200

    Added navbar store

---
+++ azure-blogify-ui/src/stores/navbar.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+export const useNavBarStore = defineStore('navbar', () => {
+  const router = useRouter()
+  const showSearchBar = ref(false)
+  const searchString = ref('')
+
+  function toggleSearchBar() {
+    showSearchBar.value = !showSearchBar.value
+  }
+
+  async function search() {
+    var searchquery = searchString.value.trim()
+    if (searchquery !== '') router.push(`/search/${searchquery}`)
+  }
+
+  return { showSearchBar, toggleSearchBar, search, searchString }
+})
